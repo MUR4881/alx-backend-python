85c039424c36265fd7f52ead3a3b1994e1634ed8
MUROBII <usmanmurabi@gmail.com>
2024-10-15 18:44:07 +0100

    Implement function annotation, with int & float:
    Write a type-annotated function floor which takes a float n as argument
    and returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# -*- encoding: utf-8 -*-
+
+""" A simple module containing a simple function
+"""
+
+
+def floor(n: float) -> int:
+    """ Removes the floating part of a fractional number
+
+    Args:
+        n: float
+
+    Returns: An integer (the float, without the decimal part)
+    """
+
+    return int(n)
