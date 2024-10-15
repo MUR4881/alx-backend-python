02a63583cb55cb0b6725f9d5dbcda99ffad0086a
MUROBII <usmanmurabi@gmail.com>
2024-10-15 20:56:57 +0100

    Define and annotate the following variables:
    with the specified values:
    
    	* a, an integer with a value of 1
    	* pi, a float with a value of 3.14
    	* i_understand_annotations, a boolean with a value of True
    	* school, a string with a value of "Holberton"

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# -*- encoding: utf-8 -*-
+
+""" A simple module demonstrating the use of annotations
+with global variables.
+
+Attributes:
+    a (int): an integer with a value of 1
+    pi (float): a float with a value of 3.14
+    i_understand_annotations (bool): a boolean with a value of True
+    school (str): A string with a value of "Holberton"
+
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
