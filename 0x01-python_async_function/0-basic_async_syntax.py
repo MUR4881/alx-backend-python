3d2c71af4bd7fa739a7b495fdaae489e186a2d24
Usman Aliyu Sayedi <sayyedi@ABDULLAHI.ABDULLAHI.com>
2024-12-05 23:30:50 +0100

    Debug and annotate properly:
    Use, random.uniform to generated random floats
    and annotate properly.

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,14 +7,17 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     '''A simple coroutine that sleeps for a random time
 
     Args:
         max_delay: The maximum delay time that defaults to 0
 
+    Returns:
+        The float the randomly generated sleep_time
+
     '''
 
-    sleep_time: int = random.randint(0, max_delay)  # the time to sleep
+    sleep_time: float = random.uniform(0, max_delay)  #: the time to sleep
     await asyncio.sleep(sleep_time)
     return sleep_time
